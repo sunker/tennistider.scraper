658c8e22db97f6203e4d0e4167d749f5da68f36f
Erik Sundell <erik.sundell87@gmail.com>
2018-10-27 18:44:27 +0200

    ref

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM justsml/docker-phantom-node
+
+WORKDIR /app
+
+COPY package.json yarn.lock ./
+
+RUN yarn install && \
+    yarn cache clean
+    
+
+COPY . .
+
+CMD ["node", "app.js"]
+
+
+# FROM mhart/alpine-node:9
+
+# WORKDIR /app
+
+# COPY package.json yarn.lock ./
+
+# RUN yarn install && \
+#     yarn cache clean
+    
+
+# COPY . .
+
+# CMD ["node", "app.js"]
--- listener.js
+++ listener.js
@@ -1,78 +1,96 @@
-const HellasClient = require('./scrapers/HellasClient')
-const MatchiClient = require('./scrapers/MatchiClient')
-const MatchiPadelClient = require('./scrapers/MatchiPadelClient')
-const EnskedeClient = require('./scrapers/EnskedeClient')
-const MyCourtClient = require('./scrapers/MyCourtClient')
-const settings = require('./settings')
-const Helper = require('./scrapers/helper')
-const rp = require('request-promise')
+const HellasClient = require("./scrapers/HellasClient");
+const MatchiClient = require("./scrapers/MatchiClient");
+const MatchiPadelClient = require("./scrapers/MatchiPadelClient");
+const EnskedeClient = require("./scrapers/EnskedeClient");
+const MyCourtClient = require("./scrapers/MyCourtClient");
+const settings = require("./settings");
+const Helper = require("./scrapers/helper");
+const rp = require("request-promise");
 
 module.exports = {
   init() {
     // const enskedeClient = new EnskedeClient()
     // enskedeClient.init()
 
-    const hellasClient = new HellasClient()
-    hellasClient.init()
-    this.initMycourt()
-    this.repeatMatchi()
-    this.repeatMatchiPadel()
+    const hellasClient = new HellasClient();
+    hellasClient.init();
+    this.initMycourt();
+    this.repeatMatchi();
+    this.repeatMatchiPadel();
   },
   async initMycourt() {
-    const myCourtClient = new MyCourtClient()
-    myCourtClient.init()
-    myCourtClient.on('slotsLoaded', ({ foundSlots, savedSlots, clubName }) => {
-      console.log(`${foundSlots} slots (${savedSlots} new) found at ${clubName}`)
-    })
+    const myCourtClient = new MyCourtClient();
+    myCourtClient.init();
+    myCourtClient.on("slotsLoaded", ({ foundSlots, savedSlots, clubName }) => {
+      console.log(
+        `${foundSlots} slots (${savedSlots} new) found at ${clubName}`
+      );
+    });
   },
   async repeatMatchi() {
-    const matchiClubs = await rp({ uri: `${process.env.API_HOST}/api/club/list-current`, json: true }).then(clubs =>
-      clubs.filter(club => club.tag === 'matchi')
-    )
+    const matchiClubs = await rp({
+      uri: `${process.env.API_HOST}/api/club/list-current`,
+      json: true
+    }).then(clubs => clubs.filter(club => club.tag === "matchi"));
     Promise.all(
       matchiClubs.map(club => {
         return new Promise(resolve => {
           const delay = {
             minDelay: settings.matchiMinDelay * matchiClubs.length,
             maxDelay: settings.matchiMaxDelay * matchiClubs.length
-          }
-          const matchiClient = new MatchiClient(club, delay)
+          };
+          const matchiClient = new MatchiClient(club, delay);
           setTimeout(
             () => matchiClient.init(),
-            Helper.randomIntFromInterval(settings.matchiMinDelay, settings.matchiMaxDelay)
-          )
-          matchiClient.on('slotsLoaded', res => {
-            console.log(`${res.foundSlots} slots (${res.savedSlots} new) found at ${club.name}`)
-            resolve()
-          })
-        })
+            Helper.randomIntFromInterval(
+              settings.matchiMinDelay,
+              settings.matchiMaxDelay
+            )
+          );
+          matchiClient.on("slotsLoaded", res => {
+            console.log(
+              `${res.foundSlots} slots (${res.savedSlots} new) found at ${
+                club.name
+              }`
+            );
+            resolve();
+          });
+        });
       }),
       () => this.repeatMatchi()
-    ).then(() => this.repeatMatchi())
+    ).then(() => this.repeatMatchi());
   },
   async repeatMatchiPadel() {
-    const matchiPadelClubs = await rp({ uri: `${process.env.API_HOST}/api/club/list-current`, json: true }).then(
-      clubs => clubs.filter(club => club.tag === 'matchipadel')
-    )
+    const matchiPadelClubs = await rp({
+      uri: `${process.env.API_HOST}/api/club/list-current`,
+      json: true
+    }).then(clubs => clubs.filter(club => club.tag === "matchipadel"));
     Promise.all(
       matchiPadelClubs.map(club => {
         return new Promise(resolve => {
           const delay = {
             minDelay: settings.matchiPadelMinDelay * matchiPadelClubs.length,
             maxDelay: settings.matchiPadelMaxDelay * matchiPadelClubs.length
-          }
-          const matchiPadelClient = new MatchiPadelClient(club, delay)
+          };
+          const matchiPadelClient = new MatchiPadelClient(club, delay);
           setTimeout(
             () => matchiPadelClient.init(),
-            Helper.randomIntFromInterval(settings.matchiPadelMinDelay, settings.matchiPadelMaxDelay)
-          )
-          matchiPadelClient.on('slotsLoaded', res => {
-            console.log(`${res.foundSlots} slots (${res.savedSlots} new) found at ${club.name}`)
-            resolve()
-          })
-        })
+            Helper.randomIntFromInterval(
+              settings.matchiPadelMinDelay,
+              settings.matchiPadelMaxDelay
+            )
+          );
+          matchiPadelClient.on("slotsLoaded", res => {
+            console.log(
+              `${res.foundSlots} slots (${res.savedSlots} new) found at ${
+                club.name
+              }`
+            );
+            resolve();
+          });
+        });
       }),
       () => this.repeatMatchiPadel()
-    ).then(() => this.repeatMatchiPadel())
+    ).then(() => this.repeatMatchiPadel());
   }
-}
+};
